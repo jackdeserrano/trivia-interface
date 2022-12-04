b82ad660a1680acd0af6d7bbcc187bbdb84b933d
jackdeserrano <jackdeserrano19@icloud.com>
2022-12-03 21:17:20 -0500

    add josh's thing for json

---
+++ to_json.js
@@ -0,0 +1,21 @@
+// taggagii
+
+const fs = require('fs');
+
+const text = fs.readFileSync('__trivia.txt', 'utf-8');
+
+const split_text = text.split('\n\n');
+
+const output = {};
+
+for (let i = 0; i < split_text.length; ++i) {
+    const question_data = split_text[i].split('\n');
+    const [question, answer, index] = question_data;
+    output[index] = {
+        question,
+        answer,
+        index,
+    };
+}
+
+fs.writeFileSync('__trivia.json', JSON.stringify(output, null, 2));
